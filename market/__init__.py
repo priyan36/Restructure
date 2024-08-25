f5c63233951cac2fde76fb5844818c6cb7f396d9
Priyan <priyadharshanr36@gmail.com>
2024-08-25 08:53:54 +0530

    Initital Commit

---
+++ market/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Python312/00Flask/FlaskMarket/market.db"
+db = SQLAlchemy(app)
+
+from market import routes
---
+++ market/models.py
@@ -0,0 +1,11 @@
+from market import db
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)   # Changed 'column' to 'Column' and 'integer' to 'Integer'
+    name = db.Column(db.String(length=30), nullable=False, unique=True)   # Changed 'string' to 'String'
+    price = db.Column(db.Integer, nullable=False)   # Changed 'integer' to 'Integer'
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)   # Changed 'string' to 'String'
+    description = db.Column(db.String(length=1000), nullable=False, unique=True)   # Changed 'string' to 'String'
+
+    def __repr__(self):
+        return f'Item {self.name}'
